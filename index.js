43bb85098779875152a09ee0e53a4effc6169b57
tolstys <tolstys@gmail.com>
2023-06-22 00:00:54 +0300

    index.html

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ bot.on("callback_query", function (query) {
         bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
     } else {
         queries[query.id] = query;
-        let gameurl = "http://tolsty.bget.ru/tiles/";
+        let gameurl = "https://github.com/tolstys/infinity_tiles";
         bot.answerCallbackQuery({
             callback_query_id: query.id,
             url: gameurl
